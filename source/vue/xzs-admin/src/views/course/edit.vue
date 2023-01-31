bc5355162d5c492ccef60b25f9b40a3d6c3e9cc9
herzshen <herzshen@tencent.com>
2023-01-31 23:31:06 +0800

    学生端新增课程

--- source/vue/xzs-admin/src/views/course/edit.vue
+++ source/vue/xzs-admin/src/views/course/edit.vue
@@ -1,49 +1,42 @@
 <template>
-  <div class="app-container">
-    <el-form :model="form" ref="form" label-width="100px" v-loading="formLoading">
-      <el-form-item label="年级：" required>
-        <el-select v-model="form.level" placeholder="年级">
-          <el-option v-for="item in levelEnum" :key="item.key" :value="item.key" :label="item.value"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="学科：" required>
-        <el-select v-model="form.level" placeholder="学科">
-          <el-option v-for="item in levelEnum" :key="item.key" :value="item.key" :label="item.value"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="课程名称：" required>
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="封面：" required>
-        <el-upload
-          action="https://jsonplaceholder.typicode.com/posts/"
-          list-type="picture-card"
-          accept=".jpg, .jpeg, .png, .JPG, .JPEG"
-          :limit="1"
-          :on-success="handleSuccess"
-          :on-remove="handleRemove">
-            <i class="el-icon-plus"></i>
-        </el-upload>
-      </el-form-item>
-      <el-form-item label="描述：" required>
-        <el-input v-model="form.name" type="textarea" :autosize="{ minRows: 2, maxRows: 4}"></el-input>
-      </el-form-item>
-      <el-form-item label="课程视频：" required>
-        <el-upload
-          action="https://jsonplaceholder.typicode.com/posts/"
-          accept=".mp4, .ogg, .flv, .avi, .wmv, .rmvb"
-          :limit="1"
-          :on-success="handleSuccess"
-          :on-remove="handleRemove">
-            <el-button size="small" type="primary">点击上传</el-button>
-        </el-upload>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm">提交</el-button>
-        <el-button @click="resetForm">重置</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+	<div class="app-container">
+		<el-form :model="form" ref="form" label-width="100px" v-loading="formLoading">
+			<el-form-item label="年级：" required>
+				<el-select v-model="form.level" placeholder="年级" @change="levelChange">
+					<el-option v-for="item in levelEnum" :key="item.key" :value="item.key" :label="item.value"></el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item label="学科：" required>
+				<el-select v-model="form.subjectId" placeholder="学科">
+					<el-option v-for="item in subjectFilter" :key="item.id" :value="item.id"
+						:label="item.name + ' ( ' + item.levelName + ' )'"></el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item label="课程名称：" required>
+				<el-input v-model="form.name"></el-input>
+			</el-form-item>
+			<el-form-item label="封面：" required>
+				<el-upload action="https://jsonplaceholder.typicode.com/posts/" list-type="picture-card"
+					accept=".jpg, .jpeg, .png, .JPG, .JPEG" :limit="1" :on-success="handleImgSuccess"
+					:on-remove="handleImgRemove">
+					<i class="el-icon-plus"></i>
+				</el-upload>
+			</el-form-item>
+			<el-form-item label="描述：" required>
+				<el-input v-model="form.name" type="textarea" :autosize="{ minRows: 2, maxRows: 4 }"></el-input>
+			</el-form-item>
+			<el-form-item label="课程视频：" required>
+				<el-upload action="https://jsonplaceholder.typicode.com/posts/" accept=".mp4, .ogg, .flv, .avi, .wmv, .rmvb"
+					:limit="1" :on-success="handleVideoSuccess" :on-remove="handleVideoRemove">
+					<el-button size="small" type="primary">点击上传</el-button>
+				</el-upload>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="submitForm">提交</el-button>
+				<el-button @click="resetForm">重置</el-button>
+			</el-form-item>
+		</el-form>
+	</div>
 </template>
 
 <script>
@@ -51,93 +44,104 @@ import { mapGetters, mapState, mapActions } from 'vuex'
 import subjectApi from '@/api/subject'
 
 export default {
-  data () {
-    return {
-      form: {
-        id: null,
-        name: '',
-        level: 1,
-        levelName: ''
-      },
-      formLoading: false
-    }
-  },
-  created () {
-    let id = this.$route.query.id
-    let _this = this
-    if (id && parseInt(id) !== 0) {
-      _this.formLoading = true
-      subjectApi.select(id).then(re => {
-        _this.form = re.response
-        _this.formLoading = false
-      })
-    }
-  },
-  methods: {
-    submitForm () {
-      let _this = this
-      this.formLoading = true
-      this.form.levelName = this.enumFormat(this.levelEnum, this.form.level)
-      subjectApi.edit(this.form).then(data => {
-        if (data.code === 1) {
-          _this.$message.success(data.message)
-          _this.delCurrentView(_this).then(() => {
-            _this.$router.push('/education/subject/list')
-          })
-        } else {
-          _this.$message.error(data.message)
-          _this.formLoading = false
-        }
-      }).catch(e => {
-        _this.formLoading = false
-      })
-    },
-    resetForm () {
-      let lastId = this.form.id
-      this.$refs['form'].resetFields()
-      this.form = {
-        id: null,
-        name: '',
-        level: 1,
-        levelName: ''
-      }
-      this.form.id = lastId
-    },
-    ...mapActions('tagsView', { delCurrentView: 'delCurrentView' })
-  },
-  computed: {
-    ...mapGetters('enumItem', [
-      'enumFormat'
-    ]),
-    ...mapState('enumItem', {
-      levelEnum: state => state.user.levelEnum
-    })
-  }
+	data() {
+		return {
+			form: {
+				id: null,
+				name: '',
+				level: 1,
+				levelName: '',
+				subjectId: ''
+			},
+			formLoading: false,
+			subjectFilter: null,
+		}
+	},
+	created() {
+		let id = this.$route.query.id
+		let _this = this
+		if (id && parseInt(id) !== 0) {
+			_this.formLoading = true
+			subjectApi.select(id).then(re => {
+				_this.form = re.response
+				_this.formLoading = false
+			})
+		}
+	},
+	methods: {
+		submitForm() {
+			let _this = this
+			this.formLoading = true
+			this.form.levelName = this.enumFormat(this.levelEnum, this.form.level)
+			subjectApi.edit(this.form).then(data => {
+				if (data.code === 1) {
+					_this.$message.success(data.message)
+					_this.delCurrentView(_this).then(() => {
+						_this.$router.push('/education/subject/list')
+					})
+				} else {
+					_this.$message.error(data.message)
+					_this.formLoading = false
+				}
+			}).catch(e => {
+				_this.formLoading = false
+			})
+		},
+		resetForm() {
+			let lastId = this.form.id
+			this.$refs['form'].resetFields()
+			this.form = {
+				id: null,
+				name: '',
+				level: 1,
+				levelName: ''
+			}
+			this.form.id = lastId
+		},
+		levelChange() {
+			this.form.subjectId = null
+			this.subjectFilter = this.subjects.filter(data => data.level === this.form.level)
+		},
+		...mapActions('tagsView', { delCurrentView: 'delCurrentView' }),
+		...mapActions('exam', { initSubject: 'initSubject' })
+	},
+	computed: {
+		...mapGetters('enumItem', [
+			'enumFormat'
+		]),
+		...mapState('enumItem', {
+			levelEnum: state => state.user.levelEnum
+		}),
+		...mapGetters('exam', ['subjectEnumFormat']),
+		...mapState('exam', { subjects: state => state.subjects })
+	}
 }
 </script>
 <style lang="scss" scoped>
-
 .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
-  }
+	border: 1px dashed #d9d9d9;
+	border-radius: 6px;
+	cursor: pointer;
+	position: relative;
+	overflow: hidden;
+}
+
+.avatar-uploader .el-upload:hover {
+	border-color: #409EFF;
+}
+
+.avatar-uploader-icon {
+	font-size: 28px;
+	color: #8c939d;
+	width: 178px;
+	height: 178px;
+	line-height: 178px;
+	text-align: center;
+}
+
+.avatar {
+	width: 178px;
+	height: 178px;
+	display: block;
+}
 </style>
--- source/vue/xzs-admin/src/views/course/list.vue
+++ source/vue/xzs-admin/src/views/course/list.vue
@@ -1,42 +1,40 @@
 <template>
-  <div class="app-container">
-    <el-form :model="queryParam" ref="queryForm" :inline="true">
-      <el-form-item label="年级：">
-        <el-select v-model="queryParam.level" placeholder="年级" @change="levelChange" clearable>
-          <el-option v-for="item in levelEnum" :key="item.key" :value="item.key" :label="item.value"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="课程分类：" >
-        <el-select v-model="queryParam.subjectId"  clearable>
-          <el-option v-for="item in subjectFilter" :key="item.id" :value="item.id" :label="item.name+' ( '+item.levelName+' )'"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="名称：">
-        <el-input v-model="queryParam.id" clearable></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm">查询</el-button>
-        <!-- <router-link :to="{path:'/edit'}" class="link-left">
-          <el-button type="primary">添加</el-button>
-        </router-link> -->
-      </el-form-item>
-    </el-form>
-    <el-table v-loading="listLoading" :data="tableData" border fit highlight-current-row style="width: 100%">
-      <el-table-column prop="id" label="Id" width="90px"/>
-      <el-table-column prop="subjectId" label="封面" :formatter="subjectFormatter" width="120px" />
-      <el-table-column prop="name" label="课程名称"  />
-      <el-table-column prop="createTime" label="分类"/>
-      <el-table-column prop="createTime" label="年级" />
-      <el-table-column  label="操作" align="center"  width="160px">
-        <template slot-scope="{row}">
-          <!-- <el-button size="mini" @click="$router.push({path:'/exam/paper/edit',query:{id:row.id}})" >编辑</el-button> -->
-          <el-button size="mini" type="danger"  @click="deletePaper(row)" class="link-left">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <pagination v-show="total>0" :total="total" :page.sync="queryParam.pageIndex" :limit.sync="queryParam.pageSize"
-                @pagination="search"/>
-  </div>
+	<div class="app-container">
+		<el-form :model="queryParam" ref="queryForm" :inline="true">
+			<el-form-item label="年级：">
+				<el-select v-model="queryParam.level" placeholder="年级" @change="levelChange" clearable>
+					<el-option v-for="item in levelEnum" :key="item.key" :value="item.key" :label="item.value"></el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item label="学科：">
+				<el-select v-model="queryParam.subjectId" clearable>
+					<el-option v-for="item in subjectFilter" :key="item.id" :value="item.id"
+						:label="item.name + ' ( ' + item.levelName + ' )'"></el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item label="名称：">
+				<el-input v-model="queryParam.id" clearable></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="submitForm">查询</el-button>
+			</el-form-item>
+		</el-form>
+		<el-table v-loading="listLoading" :data="tableData" border fit highlight-current-row style="width: 100%">
+			<el-table-column prop="id" label="Id" width="90px" />
+			<el-table-column prop="subjectId" label="封面" :formatter="subjectFormatter" width="120px" />
+			<el-table-column prop="name" label="课程名称" />
+			<el-table-column prop="createTime" label="分类" />
+			<el-table-column prop="createTime" label="年级" />
+			<el-table-column label="操作" align="center" width="160px">
+				<template slot-scope="{row}">
+					<!-- <el-button size="mini" @click="$router.push({path:'/exam/paper/edit',query:{id:row.id}})" >编辑</el-button> -->
+					<el-button size="mini" type="danger" @click="deletePaper(row)" class="link-left">删除</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+		<pagination v-show="total > 0" :total="total" :page.sync="queryParam.pageIndex" :limit.sync="queryParam.pageSize"
+			@pagination="search" />
+	</div>
 </template>
 
 <script>
@@ -45,68 +43,68 @@ import Pagination from '@/components/Pagination'
 import examPaperApi from '@/api/examPaper'
 
 export default {
-  components: { Pagination },
-  data () {
-    return {
-      queryParam: {
-        id: null,
-        level: null,
-        subjectId: null,
-        pageIndex: 1,
-        pageSize: 10
-      },
-      subjectFilter: null,
-      listLoading: true,
-      tableData: [],
-      total: 0
-    }
-  },
-  created () {
-    this.initSubject()
-    this.search()
-  },
-  methods: {
-    submitForm () {
-      this.queryParam.pageIndex = 1
-      this.search()
-    },
-    search () {
-      this.listLoading = false
-      examPaperApi.pageList(this.queryParam).then(data => {
-        const re = data.response
-        this.tableData = re.list
-        this.total = re.total
-        this.queryParam.pageIndex = re.pageNum
-        this.listLoading = false
-      })
-    },
-    deletePaper (row) {
-      let _this = this
-      examPaperApi.deletePaper(row.id).then(re => {
-        if (re.code === 1) {
-          _this.search()
-          _this.$message.success(re.message)
-        } else {
-          _this.$message.error(re.message)
-        }
-      })
-    },
-    levelChange () {
-      this.queryParam.subjectId = null
-      this.subjectFilter = this.subjects.filter(data => data.level === this.queryParam.level)
-    },
-    subjectFormatter  (row, column, cellValue, index) {
-      return this.subjectEnumFormat(cellValue)
-    },
-    ...mapActions('exam', { initSubject: 'initSubject' })
-  },
-  computed: {
-    ...mapGetters('enumItem', ['enumFormat']),
-    ...mapState('enumItem', {
-      levelEnum: state => state.user.levelEnum
-    }),
-    ...mapGetters('exam', ['subjectEnumFormat']),
-    ...mapState('exam', { subjects: state => state.subjects })
-  }
+	components: { Pagination },
+	data() {
+		return {
+			queryParam: {
+				id: null,
+				level: null,
+				subjectId: null,
+				pageIndex: 1,
+				pageSize: 10
+			},
+			subjectFilter: null,
+			listLoading: true,
+			tableData: [],
+			total: 0
+		}
+	},
+	created() {
+		this.initSubject()
+		this.search()
+	},
+	methods: {
+		submitForm() {
+			this.queryParam.pageIndex = 1
+			this.search()
+		},
+		search() {
+			this.listLoading = false
+			examPaperApi.pageList(this.queryParam).then(data => {
+				const re = data.response
+				this.tableData = re.list
+				this.total = re.total
+				this.queryParam.pageIndex = re.pageNum
+				this.listLoading = false
+			})
+		},
+		deletePaper(row) {
+			let _this = this
+			examPaperApi.deletePaper(row.id).then(re => {
+				if (re.code === 1) {
+					_this.search()
+					_this.$message.success(re.message)
+				} else {
+					_this.$message.error(re.message)
+				}
+			})
+		},
+		levelChange() {
+			this.queryParam.subjectId = null
+			this.subjectFilter = this.subjects.filter(data => data.level === this.queryParam.level)
+		},
+		subjectFormatter(row, column, cellValue, index) {
+			return this.subjectEnumFormat(cellValue)
+		},
+		...mapActions('exam', { initSubject: 'initSubject' })
+	},
+	computed: {
+		...mapGetters('enumItem', ['enumFormat']),
+		...mapState('enumItem', {
+			levelEnum: state => state.user.levelEnum
+		}),
+		...mapGetters('exam', ['subjectEnumFormat']),
+		...mapState('exam', { subjects: state => state.subjects })
+	}
 }
 </script>
